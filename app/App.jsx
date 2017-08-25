631a7ef02ce26ad95b7168d768fc9126a763b073
Boopathi <boopathi.n@knowledgeq.com>
2017-08-25 21:09:40 +0530

    Revert "Revert "eslint configuration updated.""
    This reverts commit 2af76566bd7ab97928e56f4ab9a62633a94aecf5.

--- app/App.jsx
+++ app/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
 class App extends React.Component {
-   render() {
-      return (
-         <div>
-            Sample Helloworld for React and ALT
-         </div>
-      );
-   }
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+          Sample Helloworld for React and ALT
+      </div>
+    );
+  }
 }
 
 export default App;
